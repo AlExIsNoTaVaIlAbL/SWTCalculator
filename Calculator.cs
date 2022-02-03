6bdaf158d0b39b58f170787fdc94be5e794b16d0
au681284 <au681284@uni.au.dk>
2022-02-03 13:27:55 +0100

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWTCalculator
+{
+    public class Calculator
+    {
+            public Calculator()
+            {}
+     
+            public double Add(double a, double b)
+            { return a + b; }
+
+            public double Subtract(double a, double b)
+            { return a - b; }
+
+            public double Mulitply(double a, double b)
+            { return a * b; }
+
+            public double Power(double a, double exp)
+            { return Math.Pow(a, exp); }
+    }
+
+ }
